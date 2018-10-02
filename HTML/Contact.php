38f61dd9f484ea3242532d3ff7010ac4b26fadcb
Justin Ng <JustinRiverNg@berkeley.edu>
2018-10-01 20:21:47 -0700

    style contact form

--- HTML/Contact.php
+++ HTML/Contact.php
@@ -1,6 +1,6 @@
 <?php
 if($_POST["firstname"]) {
-    mail("justinriverng@gmail.com", "Contact Form Message", $_POST["firstname"], "From: an@email.address");
+    mail("justinriverng@gmail.com", "Contact Form Message", $_POST["firstname"], $_POST["lastname"], $_POST["subject"], $_POST["replyto"]"]"From: an@email.address");
 }
 ?>
 
