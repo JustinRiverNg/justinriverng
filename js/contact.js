5c39f3e3c569dd8bed46fd1a3c2cb608837d8063
Justin Ng <justinriverng@gmail.com>
2022-02-06 22:47:32 -0800

    Add typing effect to the Contact page title text

---
+++ js/contact.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+
+    var contactTitleText = "Let's Connect!";
+    var i = 0;
+    var speed = 50;
+
+    typeOutContactTitleText();
+    
+    function typeOutContactTitleText() {
+        if (i < contactTitleText.length) {
+            document.querySelector("#contact-title-text").textContent += contactTitleText.charAt(i);
+            i++;
+            setTimeout(typeOutContactTitleText, speed);
+        }
+    }
+
+});
